5047bbffdf19bd69aa3ad11031934e51b1ce3b78
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-21 10:06:09 +0530

    Update start.py

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -15,8 +15,7 @@ async def start(event):
 
     heck = await AccGenBot(GetFullUserRequest(event.sender_id))
     name = heck.user.first_name
-    await event.reply(f"**Heya {name}\n\nWelcome to AccGenBot, From Here You can Generate free Accounts\n\nMade With ❤️ By @EvilzBots**", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
-
+    await event.reply(f"**Heya {name}\the
 #Repeat Codes :/
 @AccGenBot.on(events.callbackquery.CallbackQuery(data=b"start_bot"))
 async def start(event):
@@ -28,4 +27,4 @@ async def start(event):
 
     heck = await AccGenBot(GetFullUserRequest(event.sender_id))
     name = heck.user.first_name
-    await event.reply(f"**Heya {name}\n\nWelcome to AccGenBot, From Here You can Generate free Accounts\n\nMade With ❤️ By @EvilzBots**", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
+    await event.reply(f"**Heya {name}\n\nI am Account Generator Bot.\n\nType /cmds to know the available the Commands!\n\n❤️ I'm Made By @EvilzBots**", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
