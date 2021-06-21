59a69bcf3ad038aff8f2097e521b8a81582a9e1b
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-21 10:47:14 +0530

    Update start.py

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -7,20 +7,10 @@ from Configs import Config
 @AccGenBot.on(events.NewMessage(pattern="/start"))
 async def start(event):
 
-    evil = await verify(Config.CHANNEL_US, event, AccGenBot)
-    if evil is False:
-            await event.reply("**Join my channel to use this bot :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
-            return
-
     await event.reply(f"**Heya\n\nWelcome to AccGenBot, From Here You can Generate free Accounts\n\nMade With ❤️ By @EvilzBots**", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
 
 #Repeat Codes :/
 @AccGenBot.on(events.callbackquery.CallbackQuery(data=b"start_bot"))
 async def start(event):
-
-    evil = await verify(Config.CHANNEL_US, event, AccGenBot)
-    if evil is False:
-            await event.edit("**Join my channel to use this bot :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
-            return
 
     await event.reply(f"**Heya \n\nWelcome to AccGenBot, From Here You can Generate free Accounts\n\nMade With ❤️ By @EvilzBots**", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
