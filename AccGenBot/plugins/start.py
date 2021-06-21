67e6de65f42b18cf3290f81a7ea3e2e98ce91d09
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-21 10:08:19 +0530

    Update start.py

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -28,3 +28,15 @@ async def start(event):
     heck = await AccGenBot(GetFullUserRequest(event.sender_id))
     name = heck.user.first_name
     await event.reply(f"**Heya {name}\n\nI am Account Generator Bot.\n\nType /cmds to know the available the Commands!\n\n❤️ I'm Made By @EvilzBots**", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
+
+@AccGenBot.on(events.NewMessage(pattern="/cmds"))
+async def cmds(event):
+
+    evil = await verify(Config.CHANNEL_US, event, AccGenBot)
+    if evil is False:
+            await event.edit("**Join my channel to use this bot :)**", buttons=[[Button.url("Join Channel", Config.CHANNEL_URL)]])
+            return
+
+    heck = await AccGenBot(GetFullUserRequest(event.sender_id))
+    name = heck.user.first_name
+    await event.reply(f"**/zee5**", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
