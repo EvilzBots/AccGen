b032a1391d89b5471191778a00053d420f17da1e
EvilzBots <85481867+EvilzBots@users.noreply.github.com>
2021-06-21 10:11:02 +0530

    Update start.py

--- AccGenBot/plugins/start.py
+++ AccGenBot/plugins/start.py
@@ -15,7 +15,8 @@ async def start(event):
 
     heck = await AccGenBot(GetFullUserRequest(event.sender_id))
     name = heck.user.first_name
-    await event.reply(f"**Heya {name}\the
+    await event.reply(f"**Heya {name}\n\nI am Account Generator Bot.\n\nType /cmds to know the available the Commands!\n\n❤️ I'm Made By @EvilzBots**", buttons=[[Button.inline("Generate Accounts", data="gen")], [Button.url("Join Channel!", Config.CHANNEL_URL)]])
+
 #Repeat Codes :/
 @AccGenBot.on(events.callbackquery.CallbackQuery(data=b"start_bot"))
 async def start(event):
